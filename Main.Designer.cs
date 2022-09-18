7cc0c3eef26e898057bc1abb2b7da1bfab6f410a
Andrew Poženel <anderlli0053@live.com>
2022-09-18 04:01:20 +0200

    Linting fix

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -1,115 +1,110 @@
-﻿namespace EmptyRecycleBin
-{
-    partial class MAIN
-    {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+﻿namespace EmptyRecycleBin {
+  partial class MAIN {
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            this.DoEmpty = new System.Windows.Forms.Button();
-            this.DoExit = new System.Windows.Forms.Button();
-            this.LINK = new System.Windows.Forms.LinkLabel();
-            this.SuspendLayout();
-            // 
-            // DoEmpty
-            // 
-            this.DoEmpty.AutoSize = true;
-            this.DoEmpty.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.DoEmpty.ForeColor = System.Drawing.Color.SteelBlue;
-            this.DoEmpty.Image = global::EmptyRecycleBin.Properties.Resources.icon_scaled;
-            this.DoEmpty.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.DoEmpty.Location = new System.Drawing.Point(14, 16);
-            this.DoEmpty.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.DoEmpty.Name = "DoEmpty";
-            this.DoEmpty.Size = new System.Drawing.Size(208, 276);
-            this.DoEmpty.TabIndex = 0;
-            this.DoEmpty.Text = "Empty Recycle Bin";
-            this.DoEmpty.UseVisualStyleBackColor = true;
-            this.DoEmpty.Click += new System.EventHandler(this.DoEmpty_Click);
-            // 
-            // DoExit
-            // 
-            this.DoExit.AutoSize = true;
-            this.DoExit.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.DoExit.ForeColor = System.Drawing.Color.OrangeRed;
-            this.DoExit.Image = global::EmptyRecycleBin.Properties.Resources.icon2_scaled;
-            this.DoExit.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.DoExit.Location = new System.Drawing.Point(229, 16);
-            this.DoExit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.DoExit.Name = "DoExit";
-            this.DoExit.Size = new System.Drawing.Size(157, 276);
-            this.DoExit.TabIndex = 1;
-            this.DoExit.Text = "Quit";
-            this.DoExit.UseVisualStyleBackColor = true;
-            this.DoExit.Click += new System.EventHandler(this.DoExit_Click);
-            // 
-            // LINK
-            // 
-            this.LINK.AutoEllipsis = true;
-            this.LINK.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.LINK.Font = new System.Drawing.Font("Segoe Print", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.LINK.ForeColor = System.Drawing.Color.Red;
-            this.LINK.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
-            this.LINK.LinkColor = System.Drawing.Color.Red;
-            this.LINK.Location = new System.Drawing.Point(14, 307);
-            this.LINK.Name = "LINK";
-            this.LINK.Size = new System.Drawing.Size(371, 47);
-            this.LINK.TabIndex = 2;
-            this.LINK.TabStop = true;
-            this.LINK.Text = "Created by Andrew Poženel";
-            this.LINK.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.LINK.VisitedLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
-            this.LINK.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LINK_LinkClicked);
-            // 
-            // MAIN
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(399, 365);
-            this.ControlBox = false;
-            this.Controls.Add(this.LINK);
-            this.Controls.Add(this.DoExit);
-            this.Controls.Add(this.DoEmpty);
-            this.Cursor = System.Windows.Forms.Cursors.Cross;
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.MaximizeBox = false;
-            this.Name = "MAIN";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Empty Recycle Bin";
-            this.Load += new System.EventHandler(this.MAIN_Load);
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
-        }
-
-        #endregion
-
-        private Button DoEmpty;
-        private Button DoExit;
-        private LinkLabel LINK;
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
     }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent() {
+      this.DoEmpty = new System.Windows.Forms.Button();
+      this.DoExit = new System.Windows.Forms.Button();
+      this.LINK = new System.Windows.Forms.LinkLabel();
+      this.SuspendLayout();
+      // 
+      // DoEmpty
+      // 
+      this.DoEmpty.AutoSize = true;
+      this.DoEmpty.Font = new System.Drawing.Font("Segoe UI", 9.75 F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+      this.DoEmpty.ForeColor = System.Drawing.Color.SteelBlue;
+      this.DoEmpty.Image = global::EmptyRecycleBin.Properties.Resources.icon_scaled;
+      this.DoEmpty.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+      this.DoEmpty.Location = new System.Drawing.Point(14, 16);
+      this.DoEmpty.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+      this.DoEmpty.Name = "DoEmpty";
+      this.DoEmpty.Size = new System.Drawing.Size(208, 276);
+      this.DoEmpty.TabIndex = 0;
+      this.DoEmpty.Text = "Empty Recycle Bin";
+      this.DoEmpty.UseVisualStyleBackColor = true;
+      this.DoEmpty.Click += new System.EventHandler(this.DoEmpty_Click);
+      // 
+      // DoExit
+      // 
+      this.DoExit.AutoSize = true;
+      this.DoExit.Font = new System.Drawing.Font("Segoe UI", 9.75 F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+      this.DoExit.ForeColor = System.Drawing.Color.OrangeRed;
+      this.DoExit.Image = global::EmptyRecycleBin.Properties.Resources.icon2_scaled;
+      this.DoExit.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+      this.DoExit.Location = new System.Drawing.Point(229, 16);
+      this.DoExit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+      this.DoExit.Name = "DoExit";
+      this.DoExit.Size = new System.Drawing.Size(157, 276);
+      this.DoExit.TabIndex = 1;
+      this.DoExit.Text = "Quit";
+      this.DoExit.UseVisualStyleBackColor = true;
+      this.DoExit.Click += new System.EventHandler(this.DoExit_Click);
+      // 
+      // LINK
+      // 
+      this.LINK.AutoEllipsis = true;
+      this.LINK.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+      this.LINK.Font = new System.Drawing.Font("Segoe Print", 9.75 F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+      this.LINK.ForeColor = System.Drawing.Color.Red;
+      this.LINK.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
+      this.LINK.LinkColor = System.Drawing.Color.Red;
+      this.LINK.Location = new System.Drawing.Point(14, 307);
+      this.LINK.Name = "LINK";
+      this.LINK.Size = new System.Drawing.Size(371, 47);
+      this.LINK.TabIndex = 2;
+      this.LINK.TabStop = true;
+      this.LINK.Text = "Created by Andrew Poženel";
+      this.LINK.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+      this.LINK.VisitedLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+      this.LINK.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LINK_LinkClicked);
+      // 
+      // MAIN
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8 F, 20 F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.AutoSize = true;
+      this.ClientSize = new System.Drawing.Size(399, 365);
+      this.ControlBox = false;
+      this.Controls.Add(this.LINK);
+      this.Controls.Add(this.DoExit);
+      this.Controls.Add(this.DoEmpty);
+      this.Cursor = System.Windows.Forms.Cursors.Cross;
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+      this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+      this.MaximizeBox = false;
+      this.Name = "MAIN";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "Empty Recycle Bin";
+      this.Load += new System.EventHandler(this.MAIN_Load);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private Button DoEmpty;
+    private Button DoExit;
+    private LinkLabel LINK;
+  }
 }
--- Main.cs
+++ Main.cs
@@ -30,8 +30,6 @@
 
 // Andrew Poženel AKA SloDevTeam
 
-
-
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
@@ -41,100 +39,70 @@ using System.Linq;
 using System.Windows;
 using System.Net;
 
+namespace EmptyRecycleBin {
+  public partial class MAIN: Form
 
+  {
 
+    enum RecycleFlags: int {
+      SHRB_NOCONFIRMATION = 0x00000001, // Don't ask for confirmation
+        SHRB_NOPROGRESSUI = 0x00000001, // Don't show progress
+        SHRB_NOSOUND = 0x00000004 // Don't make sound when the action is executed
+    }
 
-namespace EmptyRecycleBin
-{
-    public partial class MAIN : Form
+    [DllImport("Shell32.dll", CharSet = CharSet.Unicode)]
+    static extern uint SHEmptyRecycleBin(IntPtr hwnd, string ? pszRootPath, RecycleFlags dwFlags);
 
-    {
+    public MAIN() {
+      InitializeComponent();
+    }
 
+    private void DoExit_Click(object sender, EventArgs e) {
+      Application.Exit();
+    }
 
-        enum RecycleFlags : int
-        {
-            SHRB_NOCONFIRMATION = 0x00000001, // Don't ask for confirmation
-            SHRB_NOPROGRESSUI = 0x00000001, // Don't show progress
-            SHRB_NOSOUND = 0x00000004 // Don't make sound when the action is executed
+    private void DoEmpty_Click(object sender, EventArgs e) {
+
+      DialogResult result;
+      result = MessageBox.Show("Are you sure you want to delete all the items in recycle bin", "Clear recycle bin", MessageBoxButtons.YesNo);
+
+      // If accepted, continue with the cleaning
+      if (result == DialogResult.Yes) {
+        try {
+          // Execute the method with the required parameters
+          uint IsSuccess = SHEmptyRecycleBin(IntPtr.Zero, null, RecycleFlags.SHRB_NOCONFIRMATION);
+          MessageBox.Show("The recycle bin has been succesfully emptied !", "Empty recycle bin", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        } catch (Exception ex) {
+          // Handle exceptions
+          MessageBox.Show("The recycle bin couldn't be emptied" + ex.Message, "Empty recycle bin", MessageBoxButtons.OK, MessageBoxIcon.Stop);
         }
 
-        [DllImport("Shell32.dll", CharSet = CharSet.Unicode)]
-        static extern uint SHEmptyRecycleBin(IntPtr hwnd, string? pszRootPath, RecycleFlags dwFlags);
-
-
-
-        public MAIN()
-        {
-            InitializeComponent();
-        }
-
-   
-
-        private void DoExit_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
-        }
-
-        private void DoEmpty_Click(object sender, EventArgs e)
-        {
-
-
-            DialogResult result;
-            result = MessageBox.Show("Are you sure you want to delete all the items in recycle bin", "Clear recycle bin", MessageBoxButtons.YesNo);
-
-            // If accepted, continue with the cleaning
-            if (result == DialogResult.Yes)
-            {
-                try
-                {
-                    // Execute the method with the required parameters
-                    uint IsSuccess = SHEmptyRecycleBin(IntPtr.Zero, null, RecycleFlags.SHRB_NOCONFIRMATION);
-                    MessageBox.Show("The recycle bin has been succesfully emptied !", "Empty recycle bin", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                }
-                catch (Exception ex)
-                {
-                    // Handle exceptions
-                    MessageBox.Show("The recycle bin couldn't be emptied" + ex.Message, "Empty recycle bin", MessageBoxButtons.OK, MessageBoxIcon.Stop);
-                }
-
-            }
-        
-
-
-
-
-            }
-
-        private void MAIN_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void LINK_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-
-            
-            try
-            {
-                VisitLink();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Unable to open link that was clicked!");
-            }
-
-        }
-
-        private void VisitLink()
-        {
-
-            System.Diagnostics.Process.Start("https://slo-dev-team.000webhostapp.com/");
-            LINK.LinkVisited = true;
-
-            //ProcessStartInfo sInfo = new ProcessStartInfo("https://slo-dev-team.000webhostapp.com/");
-            //Process.Start(sInfo);
-        }
-
+      }
 
     }
+
+    private void MAIN_Load(object sender, EventArgs e) {
+
+    }
+
+    private void LINK_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e) {
+
+      try {
+        VisitLink();
+      } catch (Exception ex) {
+        MessageBox.Show("Unable to open link that was clicked!");
+      }
+
+    }
+
+    private void VisitLink() {
+
+      System.Diagnostics.Process.Start("https://slo-dev-team.000webhostapp.com/");
+      LINK.LinkVisited = true;
+
+      //ProcessStartInfo sInfo = new ProcessStartInfo("https://slo-dev-team.000webhostapp.com/");
+      //Process.Start(sInfo);
+    }
+
+  }
 }
